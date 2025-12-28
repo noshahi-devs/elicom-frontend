d7e5dc33435e5d2c20ed2b4d64f9d375819ae1dd
sharjeelnoshahi <sharjeelnoshahi.dev@gmail.com>
2025-12-28 14:43:19 +0500

    Now Footer is complete

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -6,26 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.scss'],
 })
 export class Footer {
-  phoneNumber: string = '';
-  selectedCountryCode: string = '+92'; // default
 
   countries = [
-    { name: 'Afghanistan', code: '+93' },
-    { name: 'Albania', code: '+355' },
-    { name: 'Algeria', code: '+213' },
     { name: 'Pakistan', code: '+92' },
     { name: 'India', code: '+91' },
-    { name: 'United States', code: '+1' },
-    { name: 'United Kingdom', code: '+44' },
-    // ... baki countries
+    { name: 'USA', code: '+1' },
+    { name: 'UK', code: '+44' },
+    { name: 'Afghanistan', code: '+93' },
   ];
 
-  updatePhoneCode(event: any) {
-    const code = event.target.value;
-    this.phoneNumber = code; // input field me sirf code update
-  }
-
-  onPhoneInput(event: any) {
-    this.phoneNumber = event.target.value; // user input update
+  updatePhoneCode(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    console.log(select.value);
   }
 }
