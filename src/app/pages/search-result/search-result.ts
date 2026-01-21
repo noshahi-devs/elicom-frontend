ef5be14012301e371e37fe02cd2f263ec7e4024c
noshahi-devs <noshahidevelopersinc@gmail.com>
2026-01-21 21:35:18 +0500

    search result file create

---
+++ src/app/pages/search-result/search-result.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result',
+  imports: [],
+  templateUrl: './search-result.html',
+  styleUrl: './search-result.scss',
+})
+export class SearchResult {
+
+}
